0290d6f718c518611eff4374dd51919389a0b29b
ingrid27 <ingrid.pacheco2015@gmail.com>
2023-10-10 12:02:05 -0300

    aula01

---
+++ Carro.java
@@ -0,0 +1,12 @@
+public class Carro {
+  String nome;
+  String cor;
+  int ano;
+
+  void DadosCarro() {
+    System.out.println("nome: " + nome);
+    System.out.println("cor: " + cor);
+    System.out.println("ano: " + ano);
+  }
+  
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    Carro c1 = new Carro();
+
+    c1.nome = "Ford";
+    c1.cor = "Vermelho";
+    c1.ano = 1995;
+
+    c1.DadosCarro();
+  }
+}
